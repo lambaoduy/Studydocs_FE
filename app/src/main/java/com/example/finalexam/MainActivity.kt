1a92c1d897179256c0ab494aeadde22feaf980c0
hien <22130080@st.hcmuaf.edu.vn>
2025-06-03 00:29:43 +0700

    Feat: Thêm cấu hình cho retrofit gửi request
    - Tạo RetrofitClient.kt để cấu hình base url và tạo api
    - Tạo interface UserApi.kt để cấu hình request
    - Tạo BaseRespose.kt để nhận theo {status,message,data}
    - Tạo FollowRequest.kt để đóng gói dữ liệu gửi request

--- app/src/main/java/com/example/finalexam/MainActivity.kt
+++ app/src/main/java/com/example/finalexam/MainActivity.kt
@@ -4,16 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.finalexam.ui.theme.FinalExamTheme
 import com.example.finalexam.view.HomeScreen
-import com.google.firebase.auth.FirebaseAuth
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
