253605a9677e8ab2b0c6100e83ff68184e065595
Souma18 <22130078@st.hcmuaf.edu.vn>
2025-06-02 21:46:24 +0700

    them myLibrary

---
+++ app/src/main/java/com/example/finalexam/entity/Document.kt
@@ -0,0 +1,12 @@
+package com.example.finalexam.entity
+data class Document(
+    val id: String,
+    val title: String,
+    val author: String,
+    val createdDate: String
+
+) {
+    val shortTitle: String
+        get() = if (title.length > 10) title.take(10) + "..." else title
+}
+
---
+++ app/src/main/java/com/example/finalexam/entity/University.kt
@@ -0,0 +1,11 @@
+package com.example.finalexam.entity
+
+data class University(
+    val id: String,
+    val name: String,
+    val courses: List<String>,
+    val selectedCourseIndex: Int = 0
+) {
+    val selectedCourse: String
+        get() = courses[selectedCourseIndex]
+}
