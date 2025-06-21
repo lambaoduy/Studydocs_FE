b79506d0e1f0bbd75011e1f7af683895fd885663
SubeoHocJava <165917719+SubeoHocJava@users.noreply.github.com>
2025-06-21 08:24:59 +0700

    feat: Add UpdateProfile and LoadProfile intents, and implement user profile update and retrieval use cases

--- app/src/main/java/com/example/finalexam/intent/AuthIntent.kt
+++ app/src/main/java/com/example/finalexam/intent/AuthIntent.kt
@@ -6,4 +6,8 @@ sealed class AuthIntent {
     data class Register(val username: String, val email: String, val password: String) : AuthIntent()
     data class ForgotPassword(val email: String) : AuthIntent()
     object ClearError : AuthIntent()
+    // Intent cập nhật thông tin user
+    data class UpdateProfile(val user: com.example.finalexam.entity.User) : AuthIntent()
+    // Intent lấy lại thông tin user từ Firebase
+    data class LoadProfile(val userId: String) : AuthIntent()
 }
