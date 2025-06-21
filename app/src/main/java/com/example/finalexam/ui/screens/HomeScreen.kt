2537f5bdbbe6c1105e4c77fb1c6d01196a566b15
hien <22130080@st.hcmuaf.edu.vn>
2025-06-21 16:30:50 +0700

    Refactor: merge

--- app/src/main/java/com/example/finalexam/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/finalexam/ui/screens/HomeScreen.kt
@@ -1,27 +1,12 @@
 package com.example.finalexam.ui.screens
 
-import TopBar
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FilterList
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.finalexam.entity.Document
 import com.example.finalexam.ui.components.HomeScreen.BottomBar
 import com.example.finalexam.ui.components.HomeScreen.Content
-import com.example.finalexam.ui.components.HomeScreen.ListDocumentView
+import com.example.finalexam.ui.screens.HomeScreen.TopBar
 import com.example.finalexam.ui.theme.FinalExamTheme
 
 @Composable
@@ -31,52 +16,10 @@ fun HomeScreen() {
             topBar = { TopBar() },
             bottomBar = { BottomBar() },
             content =
-//            {padding ->
-//        ContentPreview(modifier = Modifier.padding(padding))}
-            { padding ->
-                Content(modifier = Modifier.padding(padding))
-            }
+                { padding ->
+                    Content(modifier = Modifier.padding(padding))
+                }
         )
     }
 }
-
-//@Preview(showBackground = true, widthDp = 400, heightDp = 700)
-//@Composable
-//fun HomeScreenPreview() {
-//    FinalExamTheme {
-//        Scaffold(
-//            topBar = { TopBar() },
-//            bottomBar = { BottomBar() },
-//            content = { padding ->
-//                ContentPreview(modifier = Modifier.padding(padding))
-//            }
-//        )
-//    }
-//}
-//@Composable
-//fun ContentPreview(modifier: Modifier = Modifier) {
-//    val sampleDocs = listOf(
-//        Document("1", "Lập trình Android", "CNTT", "UIT"),
-//        Document("2", "Trí tuệ nhân tạo", "Khoa học máy tính", "BK"),
-//        Document("3", "Cơ sở dữ liệu", "Hệ thống thông tin", "HCMUS")
-//    )
-//
-//    Column(modifier = modifier.padding(16.dp)) {
-//        OutlinedTextField(
-//            value = "",
-//            onValueChange = {},
-//            label = { Text("Tìm kiếm") },
-//            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search") },
-//            trailingIcon = { Icon(Icons.Default.FilterList, contentDescription = "Filter") },
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(56.dp),
-//            shape = RoundedCornerShape(12.dp)
-//        )
-//
-//        Spacer(modifier = Modifier.height(16.dp))
-//
-//        ListDocumentView(sampleDocs)
-//    }
-//}
 
--- app/src/main/java/com/example/finalexam/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/finalexam/viewmodel/HomeViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.finalexam.viewmodel
+
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.finalexam.data.api.DocumentApi
 import com.example.finalexam.data.dao.document.DocumentDao
-import com.example.finalexam.handler.HomeScreen.HomFindBySubject
-import com.example.finalexam.handler.HomeScreen.HomeFindBySchool
-import com.example.finalexam.handler.HomeScreen.HomeFindHandler
+import com.example.finalexam.handler.HomeScreen.HomeFindWithFiltersHandler
 import com.example.finalexam.handler.HomeScreen.HomeGetAllHandler
 import com.example.finalexam.handler.HomeScreen.HomeLoadByUerIDHandler
+import com.example.finalexam.handler.HomeScreen.NavigateToDocumentDetailHandler
 import com.example.finalexam.handler.IntentHandler
 import com.example.finalexam.intent.HomeIntent
 import com.example.finalexam.network.RetrofitClient
@@ -18,16 +19,14 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import android.app.Application
-import com.example.finalexam.handler.HomeScreen.HomeFindWithFiltersHandler
-import com.example.finalexam.handler.HomeScreen.NavigateToDocumentDetailHandler
 
 
 //file này duy viết
-class HomeViewModel : ViewModel() {
+class HomeViewModel(app: Application) : ViewModel() {
 
-//     biến reducer để chuyển tất cả những gì mà usecase nhận được thành state
+    //     biến reducer để chuyển tất cả những gì mà usecase nhận được thành state
     private val reducer = HomeReducer()
+
     // Đây là biến state lưu trữ trạng thái màn hình hiện tại
 //   Như là trên màn hình đang hiện sản tài liệu nào, bao nhiê tài liệu, đang ở trang tài liệu thứ mấy...
     private val _state = MutableStateFlow(HomeState(listOf()))
@@ -39,18 +38,23 @@ class HomeViewModel : ViewModel() {
 //    biến handlers sẽ lưu trữ toàn bộ hành động mà view model nó làm được
 //
 
-    private val handlers: List<IntentHandler<HomeIntent,HomeResult>> = listOf(
+    private val handlers: List<IntentHandler<HomeIntent, HomeResult>> = listOf(
         HomeLoadByUerIDHandler(documentDao),//lấy dữ liệu theo id user
         HomeGetAllHandler(documentDao),// lấy tất cả dữ liệu
         NavigateToDocumentDetailHandler(),
-        HomeFindWithFiltersHandler(documentDao,state.value.listDocument)
+        HomeFindWithFiltersHandler(documentDao, state.value.listDocument)
     )
+
     // xử lý intent truyền vào từ trang home ở đây
     fun processIntent(intent: HomeIntent) {
         viewModelScope.launch {
             val handler = handlers.find { it.canHandle(intent) }//tạo handler để xử lý
             handler?.handle(intent) {//kiểm tra intent có trong các intent xử lý được không
-                result -> _state.value = reducer.reduce(_state.value, result)//dùng reduce chuyển dữ liệu từ handler hành state và trả về
+                    result ->
+                _state.value = reducer.reduce(
+                    _state.value,
+                    result
+                )//dùng reduce chuyển dữ liệu từ handler hành state và trả về
             } ?: println("[WARN] No handler for intent: $intent")
         }
     }
