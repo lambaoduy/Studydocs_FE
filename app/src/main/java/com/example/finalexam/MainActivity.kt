a3cb34148aa1646f4b1e2e12a302c65c1dc3cb67
DuyDZ <22130057@st.hcmuaf.edu.vn>
2025-06-21 21:45:49 +0700

    finish: search

--- app/src/main/java/com/example/finalexam/MainActivity.kt
+++ app/src/main/java/com/example/finalexam/MainActivity.kt
@@ -13,7 +13,7 @@ import com.example.finalexam.data.datastore.UserProvider
 import com.example.finalexam.ui.screens.ForgotPasswordScreen
 import com.example.finalexam.ui.screens.HomeScreen
 import com.example.finalexam.ui.screens.LoginScreen
-import com.example.finalexam.ui.screens.NotificationScreen
+//import com.example.finalexam.ui.screens.NotificationScreen
 import com.example.finalexam.ui.screens.ProfileScreen
 import com.example.finalexam.ui.screens.RegisterScreen
 import com.example.finalexam.ui.theme.FinalExamTheme
@@ -25,75 +25,75 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HomeScreen()
-            FinalExamTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "login") {
-                    composable("login") {
-                        LoginScreen(
-                            onRegisterClick = { navController.navigate("register") },
-                            onForgotPasswordClick = { navController.navigate("forgot_password") },
-                            onLoginSuccess = { navController.navigate("home") }
-                        )
-                    }
-                    composable("register") {
-                        RegisterScreen(
-                            onLoginClick = {
-                                navController.popBackStack(
-                                    "login",
-                                    inclusive = false
-                                )
-                            },
-                            onRegisterSuccess = {
-                                navController.popBackStack(
-                                    "login",
-                                    inclusive = false
-                                )
-                            }
-                        )
-                    }
-                    composable("forgot_password") {
-                        ForgotPasswordScreen(
-                            onBackClick = {
-                                navController.popBackStack(
-                                    "login",
-                                    inclusive = false
-                                )
-                            },
-                            onSuccess = { navController.popBackStack("login", inclusive = false) }
-                        )
-                    }
-                    composable("home") {
-                        HomeScreen(
-                            onNavigateToDocumentDetail = { docId ->
-                                navController.navigate("document_detail/$docId")
-                            },
-                            onNavigateToRoute = {
-                                navController.navigate("profile") // 👈 điều hướng tới Profile
-                            }
-                        )
-                    }
-                    composable("profile") {
-                        ProfileScreen(
-                            userId = UserProvider.getUserId(),
-                            onEditProfile = { navController.navigate("edit_profile") }
-                        )
-                    }
-                    composable("notification") {
-                        NotificationScreen(
-                            navigateToFollow = { navController.navigate("follow") },
-                            onBackClick = { navController.popBackStack() }
-                        )
-                    }
-                    composable("follow") {
-                        FollowScreen(
-                            userId = UserProvider.getUserId(),
-                            onBackClick = { navController.popBackStack() })
-                    }
-                    composable("library") { /* LibraryScreen() */
-                        MyLibraryScreen()
-                    }
-                }
-            }
+//            FinalExamTheme {
+//                val navController = rememberNavController()
+//                NavHost(navController = navController, startDestination = "login") {
+//                    composable("login") {
+//                        LoginScreen(
+//                            onRegisterClick = { navController.navigate("register") },
+//                            onForgotPasswordClick = { navController.navigate("forgot_password") },
+//                            onLoginSuccess = { navController.navigate("home") }
+//                        )
+//                    }
+//                    composable("register") {
+//                        RegisterScreen(
+//                            onLoginClick = {
+//                                navController.popBackStack(
+//                                    "login",
+//                                    inclusive = false
+//                                )
+//                            },
+//                            onRegisterSuccess = {
+//                                navController.popBackStack(
+//                                    "login",
+//                                    inclusive = false
+//                                )
+//                            }
+//                        )
+//                    }
+//                    composable("forgot_password") {
+//                        ForgotPasswordScreen(
+//                            onBackClick = {
+//                                navController.popBackStack(
+//                                    "login",
+//                                    inclusive = false
+//                                )
+//                            },
+//                            onSuccess = { navController.popBackStack("login", inclusive = false) }
+//                        )
+//                    }
+//                    composable("home") {
+//                        HomeScreen(
+//                            onNavigateToDocumentDetail = { docId ->
+//                                navController.navigate("document_detail/$docId")
+//                            },
+//                            onNavigateToRoute = {
+//                                navController.navigate("profile") // 👈 điều hướng tới Profile
+//                            }
+//                        )
+//                    }
+//                    composable("profile") {
+//                        ProfileScreen(
+//                            userId = UserProvider.getUserId(),
+//                            onEditProfile = { navController.navigate("edit_profile") }
+//                        )
+//                    }
+//                    composable("notification") {
+//                        NotificationScreen(
+//                            navigateToFollow = { navController.navigate("follow") },
+//                            onBackClick = { navController.popBackStack() }
+//                        )
+//                    }
+//                    composable("follow") {
+//                        FollowScreen(
+//                            userId = UserProvider.getUserId(),
+//                            onBackClick = { navController.popBackStack() })
+//                    }
+//                    composable("library") { /* LibraryScreen() */
+//                        MyLibraryScreen()
+//                    }
+//                }
+//            }
         }
 
     }
