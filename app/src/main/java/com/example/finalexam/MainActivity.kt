514666d44a4dd7f1e52036b32ddf50992da452f5
DuyDZ <22130057@st.hcmuaf.edu.vn>
2025-06-22 22:22:45 +0700

    fix: my library

--- app/src/main/java/com/example/finalexam/MainActivity.kt
+++ app/src/main/java/com/example/finalexam/MainActivity.kt
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
                 val user by produceState<com.example.finalexam.entity.User?>(null) {
                     value = UserPreferences.getUser()
                 }
-                NavHost(navController = navController, startDestination = "home") {
+                NavHost(navController = navController, startDestination = startDestination) {
                     composable("login") {
                         LoginScreen(
                             onRegisterClick = { navController.navigate("register") },
